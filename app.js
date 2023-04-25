03a386ba011a15d809125b399e5c7b4a8958bc8c
pywhy1 <norskinwater000@gmail.com>
2023-04-25 17:22:54 -0400

    Picture additions
    Added hover effect, to be continued, and added pictures of projects, to be expanded and worked upon. Definitely not done but is a great start.

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+
+
+// imageHover.js
+
+// Define an array of PNG image IDs
+var pngImageIds = ["CS1", "CS2", "CS3"]; // Add or remove image IDs as needed
+
+// Loop through all the PNG image IDs
+for (var i = 0; i < pngImageIds.length; i++) {
+    // Get the PNG image element by ID
+    var pngImage = document.getElementById(pngImageIds[i]);
+
+    // Add event listener for mouseover event
+    pngImage.addEventListener("mouseover", function() {
+        // Change the z-index to a higher value
+        this.style.zIndex = "999";
+    });
+
+    // Add event listener for mouseout event
+    pngImage.addEventListener("mouseout", function() {
+        // Change the z-index back to the original value
+        this.style.zIndex = "0";
+    });
+}
+
+
+
+
+
 // Debounce function to limit the frequency of scroll event
 function debounce(func, wait = 20) {
     let timeout;
@@ -43,4 +72,5 @@ function debounce(func, wait = 20) {
       c++;
     }
   }
-  
+
+
